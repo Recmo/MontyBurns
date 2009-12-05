3aba252a891c438e6c8b1061c54235cd2dcd1342
Remco Bloemen <Remco.Bloemen@gmail.com>
2009-12-05 12:30:42 +0100

    Code cleanup.

--- monty.cpp
+++ monty.cpp
@@ -8,7 +8,7 @@ monty::monty(uint64 modulus)
 {
 	m = modulus;
 	R = max_uint64 - m + 1;
-	
+
 	// φ(m) = m - 1  (m is prime)
 	// r = R^(m-2) mod m
 	r = 1;
@@ -18,7 +18,7 @@ monty::monty(uint64 modulus)
 		if(e & 1) r = modular_mul(r, b);
 		b = modular_mul(b, b);
 	}
-	
+
 	// φ(2⁶⁴) = 2⁶³
 	// k = R^(2⁶³-1)  (implicit mod 2⁶⁴)
 	k = 1;
@@ -28,11 +28,11 @@ monty::monty(uint64 modulus)
 		if(e & 1) k *= b;
 		b *= b;
 	}
-	
+
 	/*
 	// Factor φ(m) = m - 1
 	phi_factors = prime_factors(m - 1);
-	
+
 	// Find the smalest generator
 	for(uint64 i = two(); true; i = add(i, one()))
 	{
@@ -43,7 +43,7 @@ monty::monty(uint64 modulus)
 		}
 	}
 	*/
-	
+
 	/*
 	cout << " m = " << m << endl;
 	cout << " R = " << R << endl;
@@ -80,7 +80,7 @@ uint64 monty::order(uint64 n) const
 {
 	// Start with order o = φ(m) = m - 1
 	uint64 o = m - 1;
-	
+
 	// The order must be a divisor of φ(m)
 	// so try to remove factors of φ(m)
 	for(int i=0; i < phi_factors.size(); i++)
@@ -100,7 +100,7 @@ uint64 monty::order(uint64 n) const
 			}
 		}
 	}
-	
+
 	return o;
 }
 
